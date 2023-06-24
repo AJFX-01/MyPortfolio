6808844bae5ed266e1a5ba9d3e329a5ac546b684
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-06-24 10:33:45 -0700

    second

---
+++ scripts/skill.js
@@ -0,0 +1,160 @@
+const skills =  [
+    {
+        skillName: 'Python',
+        stylewidth : '90%',
+        skillPercent : '90%'
+    },
+
+    {
+        skillName: 'JavaScript',
+        stylewidth : '85%',
+        skillPercent : '85%'
+    },
+
+    {
+        skillName: 'C',
+        stylewidth : '86%',
+        skillPercent : '86%'
+    },
+
+    {
+        skillName: 'TensorFlow',
+        stylewidth : '85%',
+        skillPercent : '85%'
+    },
+
+    {
+        skillName: 'PyTorch',
+        stylewidth : '80%',
+        skillPercent : '80%'
+    },
+
+    {
+        skillName: 'Flutter/Dart',
+        stylewidth : '76%',
+        skillPercent : '76%'
+    },
+
+    {
+        skillName: 'ReactJS',
+        stylewidth : '80%',
+        skillPercent : '80%'
+    },
+
+    {
+        skillName: 'NodeJS',
+        stylewidth : '80%',
+        skillPercent : '*)%'
+    },
+
+    {
+        skillName: 'SQL',
+        stylewidth : '95%',
+        skillPercent : '95%'
+    },
+
+    {
+        skillName: 'Django',
+        stylewidth : '70%',
+        skillPercent : '70%'
+    },
+    
+    {
+        skillName: 'Flask',
+        stylewidth : '82%',
+        skillPercent : '82%'
+    },
+
+    {
+        skillName: 'HTML/CSS',
+        stylewidth : '90%',
+        skillPercent : '90%'
+    },
+];
+
+
+const portFolio = [
+    {
+        imageData: '',
+        linkBtn: '',
+    },
+
+    {
+        imageData: '',
+        linkBtn: '',
+    },
+
+    {
+        imageData: '',
+        linkBtn: '',
+    },
+
+    {
+        imageData: '',
+        linkBtn: '',
+    },
+
+    {
+        imageData: '',
+        linkBtn: '',
+    },
+
+    {
+        imageData: '',
+        linkBtn: '',
+    }
+];
+
+const education = [
+    {
+        dataData : 'Oct 2016 - Dec 2022',
+        degree: '>Bachelor in Mechanical Engineering',
+        detailsInfo : 'I graduated with a first-class honors degree in Mechanical Engineering,achieving a GPA of 4.59 out of 5. My educational journey was both rewarding and enjoyable,providing me with a solid foundation in the field. Throughout my studies,I gained valuable theoretical knowledge and practical skills, engaging in hands-on projects and problem-solving activities.'
+    },
+
+    {
+        dataData: 'Mar 2023 - April 2023',
+        degree: 'Harvard University CS50p',
+        detailsInfo: 'CS50 Python: Completed Harvard\'s online course focused on Python programming, Covered fundamentals like variables, loops, functions, and data structures. Gained hands-on experiencethrough problem sets and assignments. Equipped with a solid foundation in Python for further explorationin computer science and software development.'
+    },
+
+    
+    {
+        dataData: 'Sept 2022 - Oct 2022',
+        degree: 'CS50x, Harvard University\'s renowned online computer science course, completed.Explored fundamental concepts including algorithms, data structures, web development, and more.Proficient in programming languages such as C, Python, HTML, CSS, and JavaScript.Self-paced learner with a verified certificate of completion.',
+        detailsInfo: 'Harvard University CS50x'
+    },
+
+    
+    // {
+    //     dataData:
+    //     degree:
+    //     detailsInfo:
+    // }
+];
+
+const services = [
+    {
+        icond : 'fa fa-laptop-code',
+        title : 'Web Development',
+        deets : 'Fully responsive web development both on the frontend'
+    },
+
+    {
+        icond : 'fa fa-database',
+        title : 'Deep Learning',
+        deets : 'Neural Network traning and model traning. supervised and unsupervised machine learning'
+    },
+
+    {
+        icond : 'fa fa-server',
+        title : 'Machine Learning',
+        deets : 'Cross-platform mobile apps developments, IOs and Andriods'
+    },
+
+    {
+        icond : 'fa fa-palette',
+        title : 'Data Analysis',
+        deets : 'Big Data cleaning, scrubing'
+    }
+] 
