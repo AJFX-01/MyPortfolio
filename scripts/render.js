3bf6e15c62b0bb7b8c42baa45cb6b95e0401ddf6
Ajegbomogun Opeyemi Ayomikun <109483854+AJFX-01@users.noreply.github.com>
2023-08-10 13:04:45 -0700

    Update render.js.3

--- scripts/render.js
+++ scripts/render.js
@@ -101,7 +101,7 @@ function renderPortFolio() {
                 <div class="portfolio-img">
                     <img src="${portf.imageData}" alt="" srcset="">
                 </div>
-                <a href="${portf.linkBtn}">${port.desc}</a>
+                <a href="${portf.linkBtn}">${portf.desc}</a>
             </div>
         </div>`
     });
